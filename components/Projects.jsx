62e36c43f9ef0f42ae3db47627ef364a49e393c7
aurora-721 <aurora.makovac@gmail.com>
2022-12-19 09:04:49 +0100

    adding project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 import React from 'react';
 import todovueImg from '../public/assets/projects/todovue.png';
 import ggQuest from '../public/assets/projects/ggQuest.png'
-import vuextodoImg from '../public/assets/projects/vuextodo.png'
+import vuextodoImg from '../public/assets/projects/vuextodo.png';
+import itechWebGUI from '../public/assets/projects/black-gui2.png';
 import web3RSVP from '../public/assets/projects/web3RSVP.png'
 import fetchpeople from '../public/assets/projects/fetchpeople.png'
 import ProjectItem from './ProjectItem';
@@ -18,6 +19,12 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
+            title='I-Tech Web GUI'
+            backgroundImg={itechWebGUI}
+            projectUrl='/netflix'
+            tech='Vue Vuex JS'
+          />
+          <ProjectItem
             title='Vue To Do App'
             backgroundImg={todovueImg}
             projectUrl='https://task-tracker-vue-umber.vercel.app/'
@@ -30,13 +37,7 @@ const Projects = () => {
             tech='React, JS, Solidity, The Graph, IPFS, Infura, Polygon, Lens'
 
           />
-          <ProjectItem
-            title='Vuex To Do App'
-            backgroundImg={vuextodoImg}
-            projectUrl='https://vuextodoapp.vercel.app/'
-            tech='Vue Vuex JS'
 
-          />
           <ProjectItem
             title='ggQuest'
             backgroundImg={ggQuest}
